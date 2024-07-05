3f9cc6468990f2e1587a153f7b9137e6c433e29b
James Pae <jameslind01@yahoo.co.uk>
2024-07-05 18:18:29 +0200

    Reformatting for web DB switch

--- tool/cli.py
+++ tool/cli.py
@@ -1,14 +1,18 @@
-import duckdb
+import os
+import pyarrow as pa
+from pyarrow import dataset, json as pajson
 
 import typer
 
 app = typer.Typer()
 
 @app.command()
-def build(datadir: str, dist: str):
-    db = duckdb.read_json(f"{datadir}/*.json")
+def build():
+    table = pajson.read_json(f"{os.path.dirname(__file__)}/../data/media.json")
 
-    db.write_parquet(f"{dist}/data.paraquet")
+    ds = dataset.dataset(table)
+
+    dataset.write_dataset(ds, f"{os.path.dirname(__file__)}/../dist", format="arrow", schema=ds.schema)
 
 if __name__ == "__main__":
     app()
